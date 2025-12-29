28104c449cfda275b93eed783c34419bce7e745f
Whsxg <shabanov.matveyka@bk.ru>
2025-12-29 10:17:58 +0300

    Update text.js

--- composer/text.js
+++ composer/text.js
@@ -3,7 +3,7 @@ const { Composer } = require('telegraf');
 
 const composer = new Composer();
 
-const forwardChatId = '-1002566601942'; // ваш чат для пересылки
+const forwardChatId = '-1003618316671'; // ваш чат для пересылки
 
 composer.on('message', async (ctx) => {
     console.log('Получено сообщение:', ctx.message);
